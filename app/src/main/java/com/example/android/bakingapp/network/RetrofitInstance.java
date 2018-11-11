3e62c5723ec37ae0fee24c8560813ffe6f948d02
Mohammad Odeh <oude.moh@gmail.com>
2018-11-11 09:08:17 +0200

    Fixing SSL issue

--- app/src/main/java/com/example/android/bakingapp/network/RetrofitInstance.java
+++ app/src/main/java/com/example/android/bakingapp/network/RetrofitInstance.java
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitInstance {
     private static Retrofit retrofit;
     private static final String BASE_URL =
-            "https://d17h27t6h515a5.cloudfront.net/topher/" +
+            "http://d17h27t6h515a5.cloudfront.net/topher/" +
                     "2017/May/59121517_baking/";
 
     public static Retrofit getRetrofitInstance() {
